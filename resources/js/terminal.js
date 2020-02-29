7bf547adf6ade056028bb3d00d359d302b693cf8
Medard Mandane <medardm13@gmail.com>
2020-02-29 21:26:41 +0800

    Separate terminal js initialization from app.js

--- resources/js/terminal.js
+++ resources/js/terminal.js
@@ -1,19 +1,21 @@
+require('terminal.js')
 import hljs from 'highlight.js';
 import Typed from 'typed.js';
-require('terminal.js')
 
-// hljs.initHighlightingOnLoad();
-hljs.configure({useBR: true});
+$(document).ready(function(){
+    // hljs.initHighlightingOnLoad();
+    hljs.configure({useBR: true});
 
-document.querySelectorAll('.code').forEach((block) => {
-  hljs.highlightBlock(block);
+    document.querySelectorAll('.code').forEach((block) => {
+      hljs.highlightBlock(block);
+    });
+
+    if ($('#typed').length != 0) {
+      var typed = new Typed('#typed', {
+        stringsElement: '#typed-strings',
+        backDelay: 3000,
+        loop:true,
+        typeSpeed:10
+      });
+    }
 });
-
-if ($('#typed').length != 0) {
-  var typed = new Typed('#typed', {
-    stringsElement: '#typed-strings',
-    backDelay: 3000,
-    loop:true,
-    typeSpeed:10
-  });
-}
